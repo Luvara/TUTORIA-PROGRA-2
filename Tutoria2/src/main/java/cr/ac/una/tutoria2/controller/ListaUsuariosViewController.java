187ba3b82c9a8700e7370a1d289acf31c6345595
Luvara <luvarapz@gmail.com>
2024-04-09 13:12:43 -0600

    Tutoria 6
    Se hizo un ejercicio de practica de clases
    además se vio la creación de botones desde código, asignar estilos desde código, acciones de clic y crear botones dependiendo el rol de usuario

---
+++ Tutoria2/src/main/java/cr/ac/una/tutoria2/controller/ListaUsuariosViewController.java
@@ -0,0 +1,79 @@
+package cr.ac.una.tutoria2.controller;
+
+import cr.ac.una.tutoria2.model.Usuario;
+import cr.ac.una.tutoria2.util.AppContext;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO PZ UNA
+ */
+public class ListaUsuariosViewController extends Controller implements Initializable {
+
+    @FXML
+    private TableView<Usuario> tbvListaUsuarios;
+    @FXML
+    private TableColumn<Usuario, String> tbcCedula;
+    @FXML
+    private TableColumn<Usuario, String> tbcNombre;
+    @FXML
+    private TableColumn<Usuario, String> tbcPriApellido;
+    @FXML
+    private TableColumn<Usuario, String> tbcSegApellido;
+
+    List<Usuario> listaUsuarios = new ArrayList<>();
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        anadirSlots();
+        
+        listaUsuarios = (List<Usuario>) AppContext.getInstance().get("LISTAUSUARIOS");
+
+        ObservableList<Usuario> listaUsuariosObservable = FXCollections.observableArrayList(listaUsuarios);
+
+        tbvListaUsuarios.setItems(listaUsuariosObservable);
+        tbvListaUsuarios.refresh();
+
+        
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    public void anadirSlots() {
+
+        tbvListaUsuarios.getItems().clear();
+
+        tbcCedula.setCellValueFactory(cd -> {
+            return new SimpleStringProperty(cd.getValue().getCedula());
+        });
+        tbcNombre.setCellValueFactory(cd -> {
+            return new SimpleStringProperty(cd.getValue().getNombre());
+        });
+        tbcPriApellido.setCellValueFactory(cd -> {
+            return new SimpleStringProperty(cd.getValue().getPrimerApellido());
+        });
+        tbcSegApellido.setCellValueFactory(cd -> {
+            return new SimpleStringProperty(cd.getValue().getSegundoApellido());
+        });
+
+        tbvListaUsuarios.refresh();
+    }
+}
--- Tutoria2/src/main/java/cr/ac/una/tutoria2/controller/PrincipalView2Controller.java
+++ Tutoria2/src/main/java/cr/ac/una/tutoria2/controller/PrincipalView2Controller.java
@@ -47,94 +47,84 @@ public class PrincipalView2Controller extends Controller implements Initializabl
 
         lblUserName.setText("Bienvenido: " + user.getNombreCompleto());
 
-//        isAdmin();
+        isAdmin();
     }
 
     @Override
     public void initialize() {
-       
+
     }
 
     @FXML
     private void onActionBtnCerrarSesion(ActionEvent event) {
-//        FlowController.getInstance().goViewInWindow("logInView");
-//        FlowController.getInstance().salir();
+        FlowController.getInstance().goViewInWindow("logInView");
+        FlowController.getInstance().salir();
     }
 
     @FXML
     private void onActionBtnSalir(ActionEvent event) {
-//        FlowController.getInstance().salir();
+        FlowController.getInstance().salir();
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    private void isAdmin() {
+
+        if (user.getIsAdmin()) {
+            crearButtonsAdmin();
+        } else {
+            crearBotonesUsuario();
+        }
+    }
+
+    private void crearButtonsAdmin() {
+        Button btnMantGeneral = new Button("Mantenimiento Generales");
+        btnMantGeneral.getStyleClass().add("btnMenu");
+        btnMantGeneral.setOnAction(event -> {
+            //new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de mantenimiento general");
+
+            FlowController.getInstance().goView("ListaUsuariosView");
+        });
+
+        Button btnMantUsuarios = new Button("Mantenimiento Usuarios");
+        btnMantUsuarios.getStyleClass().add("btnMenu");
+        btnMantUsuarios.setOnAction(event -> {
+            new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de mantenimiento de usuarios");
+        });
+
+        Button btnMantReportes = new Button("Reportes");
+        btnMantReportes.getStyleClass().add("btnMenu");
+        btnMantReportes.setOnAction(event -> {
+            new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de reportes");
+        });
+
+        vboxButtons.getChildren().clear();
+        vboxButtons.getChildren().addAll(btnMantGeneral, btnMantUsuarios, btnMantReportes);
+        vboxButtons.getStyleClass().add("vbox-principal-view");
+    }
+
+    private void crearBotonesUsuario() {
+        Button btnListaUsuario = new Button("Lista Usuarios");
+        btnListaUsuario.getStyleClass().add("btnMenu");
+        btnListaUsuario.setOnAction(event -> {
+            new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de Lista de Usuarios");
+        });
+
+        Button btnBuscarUsuario = new Button("Buscar Usuario");
+        btnBuscarUsuario.getStyleClass().add("btnMenu");
+        btnBuscarUsuario.setOnAction(event -> {
+            new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de buscar usuarios");
+        });
+
+        Button btnImprimirUsuario = new Button("Imprimir Usuario");
+        btnImprimirUsuario.getStyleClass().add("btnMenu");
+        btnImprimirUsuario.setOnAction(event -> {
+            new Mensaje().showModal(Alert.AlertType.INFORMATION, "Click", stage, "Se mostraria la pantalla de impriprir un usuario");
+        });
+
+        vboxButtons.getChildren().clear();
+        vboxButtons.getChildren().addAll(btnListaUsuario, btnBuscarUsuario, btnImprimirUsuario);
+        vboxButtons.getStyleClass().add("vbox-principal-view");
+    }
+
 //    public void initialize(URL url, ResourceBundle rb) {
 //        // TODO
 //        user = (Usuario) AppContext.getInstance().get("USUARIO");
@@ -148,7 +138,6 @@ public class PrincipalView2Controller extends Controller implements Initializabl
 //    public void initialize() {
 //        isAdmin();
 //    }
-
 //    private void isAdmin() {
 //        if (user.getIsAdmin()) {
 //            crearBotonesAdmin();
@@ -206,5 +195,4 @@ public class PrincipalView2Controller extends Controller implements Initializabl
 //        vboxButtons.getChildren().addAll(btnListaUsuario, btnBuscarUsuario, btnImprimirUsuario);
 //        vboxButtons.getStyleClass().add("vbox-principal-view");
 //    }
-
 }
